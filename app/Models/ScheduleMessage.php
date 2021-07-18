3fcdd6edbd6f16b48196e0feac3c91c69ac70a1e
ywlu <ywlust+wsl2@gmail.com>
2021-07-18 12:12:06 +0800

    feat: 調整夢境連結提醒訊息的時間顯示

--- app/Models/ScheduleMessage.php
+++ app/Models/ScheduleMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
@@ -28,7 +29,23 @@ class ScheduleMessage extends Model
 
     public function getContentAttribute(): string
     {
-        return $this->attributes['content'] ?? "**`體力注意報`** 請登入領取您的體力支援:tada: @" . date('H:00');
+        $start = '00:00';
+        $end = '23:59';
+
+        $illusionTime = [
+            ['start' => '11:00', 'end' => '14:00'],
+            ['start' => '17:00', 'end' => '20:00'],
+            ['start' => '21:00', 'end' => '23:00'],
+        ];
+
+        foreach ($illusionTime as $time) {
+            extract($time);
+            if (Carbon::now()->between($start, $end)) {
+                break;
+            }
+        }
+
+        return $this->attributes['content'] ?? "**`體力注意報`** 請登入領取您的體力支援:tada: @$start ~ $end";
     }
 
     public function setContentAttribute(string $content)
